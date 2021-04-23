580dd29599390a1fd910449a172b65562b6300ff
Filipe Comastri <ffcmail.fc@gmail.com>
2021-04-23 15:22:06 -0300

    Criacao do dto com informacao do GitHub

---
+++ src/main/java/br/com/trustly/util/ScrapingUtil.java
@@ -0,0 +1,12 @@
+package br.com.trustly.util;
+
+public class ScrapingUtil {
+
+	private static final String BASE_URL_GITHUB = "https://www.github.com";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
